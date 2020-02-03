5b75ff62c694abce2335b6ca9306697e50cad0f6
Gabriele <gabrivolpe95@gmail.com>
2020-02-03 10:31:41 +0100

    test on person detectinos with yolo

--- YOLO/yolo_detect.py
+++ YOLO/yolo_detect.py
@@ -23,7 +23,7 @@ from matplotlib.ticker import NullLocator
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--image_folder", type=str, default="Test", help="path to dataset")
+    parser.add_argument("--image_folder", type=str, default="mirto/YOLO/Test", help="path to dataset")
     parser.add_argument("--model_def", type=str, default="mirto/YOLO/cfg/yolov3.cfg", help="path to model definition file")
     parser.add_argument("--weights_path", type=str, default="yolov3.weights", help="path to weights file")
     parser.add_argument("--class_path", type=str, default="mirto/YOLO/coco.names", help="path to class label file")

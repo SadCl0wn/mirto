686fb58a70023abd325b1f4db6fec46ac3d26cea
Gabriele <gabrivolpe95@gmail.com>
2020-02-03 10:29:19 +0100

    test on person detectinos with yolo

--- YOLO/yolo_detect.py
+++ YOLO/yolo_detect.py
@@ -25,7 +25,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--image_folder", type=str, default="Test", help="path to dataset")
     parser.add_argument("--model_def", type=str, default="mirto/YOLO/cfg/yolov3.cfg", help="path to model definition file")
-    parser.add_argument("--weights_path", type=str, default="sample_data/yolov3.weights", help="path to weights file")
+    parser.add_argument("--weights_path", type=str, default="yolov3.weights", help="path to weights file")
     parser.add_argument("--class_path", type=str, default="mirto/YOLO/coco.names", help="path to class label file")
     parser.add_argument("--conf_thres", type=float, default=0.8, help="object confidence threshold")
     parser.add_argument("--nms_thres", type=float, default=0.4, help="iou thresshold for non-maximum suppression")
